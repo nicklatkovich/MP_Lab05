62bcf717cb1da388773129e723e0633a9d4c0ab8
NickLatkovich <nicklatkovich@gmail.com>
2017-06-12 03:49:04 +0300

    Update AMatrix

--- MP_Lab05/AMatrix.h
+++ MP_Lab05/AMatrix.h
@@ -6,17 +6,34 @@ private:
 	T** _arr;
 	unsigned int _width;
 	unsigned int _height;
-public:
-	AMatrix(unsigned int width, unsigned int height, T defaultValue = default(T)) {
-		_width = width;
-		_height = height;
+	void Clear() {
+		for (unsigned int i = 0; i < GetWidth(); i++) {
+			delete[] _arr[i];
+		}
+		delete[] _arr;
+	}
+	void Init(unsigned int width, unsigned int height) {
+		_width = copy.GetWidth();
+		_height = copy.GetHeight();
 		_arr = new T*[GetWidth()];
 		for (unsigned int i = 0; i < GetWidth(); i++) {
 			_arr = new T[GetHeight()];
+		}
+	}
+public:
+	AMatrix(unsigned int width, unsigned int height, T defaultValue = default(T)) {
+		Init(width, height);
+		for (unsigned int i = 0; i < GetWidth(); i++) {
 			for (unsigned int j = 0; j < GetHeight(); j++) {
 				Set(i, j, defaultValue);
 			}
 		}
+	}
+	~AMatrix() {
+		Clear();
+	}
+	AMatrix(AMatrix<T>& copy) {
+		this = copy;
 	}
 	unsigned int GetWidth() {
 		return _width;
@@ -29,5 +46,14 @@ public:
 	}
 	T Get(unsigned int x, unsigned int y) {
 		return _arr[x][y];
+	}
+	AMatrix<T>& operator = (AMatrix<T>& copy) {
+		Clear();
+		Init(copy.GetWidth(), copy.GetHeight());
+		for (unsigned int i = 0; i < GetWidth(); i++) {
+			for (unsigned int j = 0; j < GetHeight(); j++) {
+				Set(i, j, copy.Get(i, j));
+			}
+		}
 	}
 };
